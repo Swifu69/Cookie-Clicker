ab8e5c0c71b9ecc207b02046c2896bdd92ee3dde
Johan Borgersen <75065342+Swifu69@users.noreply.github.com>
2021-06-07 22:49:06 +0200

    Update codes.js

--- codes/codes.js
+++ codes/codes.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+let cookies = 0;
+let cpc = 1;
+
+function bakeCookie(){
+    if(cookies <= 99){
+        cookies += cpc;
+    document.getElementById('cookie-number').innerHTML = "Cookies: " + cookies; 
+    }else{
+        alert('Ikke mer kjeks til deg')
+        location.reload();
+    }
+    
+
+}
